7aa05a6af4fd12ac4612c52edd559e7b933904ea
Paweł Morawczyński <burciak01@gmail.com>
2024-05-31 11:32:11 +0200

    init quiz server project

---
+++ QuizServer/src/main/java/com/quizserver/QuizServerApplication.java
@@ -0,0 +1,13 @@
+package com.quizserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizServerApplication.class, args);
+    }
+
+}
---
+++ QuizServer/src/test/java/com/quizserver/QuizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
